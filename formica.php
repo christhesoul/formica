21a3cb631f1c7496172934c296f9445313d3e872
Chris Waters <chris@first65.com>
2014-06-02 16:40:48 +0100

    moving to src

--- formica.php
+++
@@ -1,8 +0,0 @@
-<?php
-function FormicaAutoload($class) {
-  $classfile = dirname(__FILE__).'/classes/'.strtolower($class).'.php';
-  if(file_exists($classfile)){
-    include $classfile;
-  }
-} // function TFSLAutoload
-spl_autoload_register('FormicaAutoload'); 
--- views/inputs/checkbox.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-  <div class="col-sm-offset-3 col-sm-9">
-    <div class="checkbox">
-      <label>
-        <input type="checkbox" name="<?= $this->name ?>" value="1"> <?= $this->label ?>
-      </label>
-    </div>
-  </div>
-</div>
