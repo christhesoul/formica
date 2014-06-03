67ddb1f12f38510df5d47613b9967705a20bc9a2
Chris Waters <chris@first65.com>
2014-06-03 13:42:22 +0100

    adding initial tests and readme

--- README.md
+++ README.md
@@ -2,4 +2,25 @@ Formica
 =======
 
 Formica is a PHP thing for submitting forms and capturing data. It is my first foray into Open Source. I don't recommend using it.
-----------------------------------------------------------------------------------------------------------------------------------
+
+Getting started
+---------------
+
+Add the following to your `composer.json` and `$ composer update` as per usual.
+
+```
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/christhesoul/formica"
+  }
+],
+"require": {
+  "christhesoul/formica": "dev-master"
+}
+
+API
+---
+
+todo
+
--- tests/FormTest.php
+++ tests/FormTest.php
@@ -1,16 +1,38 @@
 <?php
 class FormTest extends PHPUnit_Framework_TestCase
 {
-    public function test_form_has_correct_default_options()
-    {
-        // Arrange
-        $form = new \Formica\Form(
-          array(
-            new \Formica\Input('my_email','Email')
-          )
-        );
-        // Assert
-        $this->assertEquals($form->method, 'post');
-        $this->assertEquals($form->action, '/');
-    }
+  public function test_construct(){
+    // Arrange
+    $form = new \Formica\Form(
+      array(
+        new \Formica\Input('my_email','Email')
+      )
+    );
+    // Assert
+    $this->assertCount(1, $form->inputs);
+  }
+  
+  public function test_process_options()
+  {
+    // Arrange
+    $form = new \Formica\Form(
+      array(
+        new \Formica\Input('my_email','Email')
+      )
+    );
+    $form_without_defaults = new \Formica\Form(
+      array(
+        new \Formica\Input('my_email','Email')
+      ),
+      array(
+        'method' => 'get'
+      )
+    );
+    // Assert
+    $this->assertEquals($form->method, 'post');
+    $this->assertEquals($form->action, '/');
+    $this->assertEquals($form_without_defaults->method, 'get');
+  }
+  
+  
 }
